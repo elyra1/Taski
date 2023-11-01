6e4e98de985f568ca56d78a79dba9420a08275b4
Даниил Никитин <05092005daniil@mail.ru>
2023-11-01 14:19:05 +0300

    Добавил цвета.

--- lib/presentation/utils/app_colors.dart
+++ lib/presentation/utils/app_colors.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
 
 abstract class AppColors {
-  static Color blue = Color(0xFF162A48);
-  static Color red = Colors.redAccent;
+  static Color basewhite = const Color(0xFFfcfcfc);
+  static Color headblue = const Color(0xFF162a48);
+  static Color textblue = const Color(0xFF20516a);
+  static Color elementblue = const Color(0xFF2a798d);
+  static Color iconblue = const Color(0xFF34a1b0);
+  static Color lightblue = const Color(0xFFd4eaed);
+  static Color grey = const Color(0xFF97a0ad);
+  static Color red = const Color(0xFFFF5B57);
+  static Color softtred = const Color(0xFFFF8671);
+  static Color pastelred = const Color(0xFFFFb0a7);
+  static Color orange = const Color(0xFFFF9742);
+  static Color softorange = const Color(0xFFFFb637);
+  static Color pastelprange = const Color(0xFFFFd4ad);
+  static Color green = const Color(0xFF47DDBF);
+  static Color softgreen = const Color(0xFF85e5cf);
+  static Color pastelgreen = const Color(0xFFb7f0e4);
+  static Color blue = const Color(0xFF5EAEF7);
+  static Color softblue = const Color(0xFF8abcd8);
+  static Color pastelblue = const Color(0xFFb8d2ec);
 }
