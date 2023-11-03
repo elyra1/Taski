6fa499c802cf423247370408d054fd8ba13608aa
vgartg <gopik539@mail.ru>
2023-11-03 16:58:28 +0300

    Добавил шрифты

--- lib/presentation/utils/app_text_styles.dart
+++ lib/presentation/utils/app_text_styles.dart
@@ -1,1 +1,47 @@
-abstract class AppTextStyles {}
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:taski/presentation/utils/app_colors.dart';
+
+abstract class AppTextStyles {
+  static TextStyle bold18 = TextStyle(
+    fontFamily: 'Montserrat',
+    fontSize: 18.sp,
+    fontWeight: FontWeight.w700,
+    color: AppColors.headblue,
+  );
+
+  static TextStyle bold20 = TextStyle(
+    fontFamily: 'Montserrat',
+    fontSize: 20.sp,
+    fontWeight: FontWeight.w700,
+    color: AppColors.headblue,
+  );
+
+  static TextStyle bold24 = TextStyle(
+    fontFamily: 'Montserrat',
+    fontSize: 24.sp,
+    fontWeight: FontWeight.w700,
+    color: AppColors.headblue,
+  );
+
+  static TextStyle regular18 = TextStyle(
+    fontFamily: 'Montserrat',
+    fontSize: 18.sp,
+    fontWeight: FontWeight.w400,
+    color: AppColors.headblue,
+  );
+
+  static TextStyle regular20 = TextStyle(
+    fontFamily: 'Montserrat',
+    fontSize: 20.sp,
+    fontWeight: FontWeight.w400,
+    color: AppColors.headblue,
+  );
+
+  static TextStyle regular24 = TextStyle(
+    fontFamily: 'Montserrat',
+    fontSize: 24.sp,
+    fontWeight: FontWeight.w400,
+    color: AppColors.headblue,
+  );
+}
