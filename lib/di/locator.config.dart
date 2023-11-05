e4671904409a91f9a15d91d6a387a081dc453ef9
elyra1 <afonchenkov98@bk.ru>
2023-11-05 07:56:17 +0300

    Добавил модель задачи

--- lib/di/locator.config.dart
+++ lib/di/locator.config.dart
@@ -12,7 +12,8 @@ import 'package:cloud_firestore/cloud_firestore.dart' as _i4;
 import 'package:firebase_auth/firebase_auth.dart' as _i3;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
-import 'package:taski/di/app_module.dart' as _i5;
+import 'package:taski/data/datasources/tasks_data_sourse.dart' as _i5;
+import 'package:taski/di/app_module.dart' as _i6;
 
 extension GetItInjectableX on _i1.GetIt {
 // initializes the registration of main-scope dependencies inside of GetIt
@@ -28,8 +29,10 @@ extension GetItInjectableX on _i1.GetIt {
     final appModule = _$AppModule();
     gh.singleton<_i3.FirebaseAuth>(appModule.firebaseAuth());
     gh.singleton<_i4.FirebaseFirestore>(appModule.fireStore());
+    gh.singleton<_i5.TaskDataSource>(
+        _i5.TaskDataSource(firebaseFirestore: gh<_i4.FirebaseFirestore>()));
     return this;
   }
 }
 
-class _$AppModule extends _i5.AppModule {}
+class _$AppModule extends _i6.AppModule {}
--- lib/domain/entities/task_entity.dart
+++ lib/domain/entities/task_entity.dart
@@ -1,4 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:taski/presentation/utils/app_colors.dart';
+part 'task_entity.freezed.dart';
 
-// @freezed
-// class TaskEntity extends _$TaskEntity {}
+@freezed
+class TaskEntity with _$TaskEntity {
+  const factory TaskEntity({
+    required String title,
+    @Default('') String? description,
+    required String id,
+    required String authorId,
+    required Timestamp startTime,
+    required Timestamp endTime,
+    required int color,
+  }) = _TaskEntity;
+
+  static TaskEntity getEmpty() => TaskEntity(
+        title: 'Название задачи',
+        id: '123',
+        authorId: '4234234',
+        startTime: Timestamp.fromDate(DateTime.now()),
+        endTime: Timestamp.fromDate(DateTime.now()),
+        color: AppColors.blue.value,
+      );
+}
