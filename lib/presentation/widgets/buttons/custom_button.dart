a03127380a56c6417edda72e5e29d6318d9b13ae
skufilova <alexahariwer@gmail.com>
2023-11-11 18:37:22 +0300

    исправила недочёты  в custom button

---
+++ lib/presentation/widgets/buttons/custom_button.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:taski/presentation/utils/app_colors.dart';
+import 'package:taski/presentation/utils/app_text_styles.dart';
+
+class CustomButton extends StatelessWidget {
+  final double width;
+  final double height;
+  final Color color;
+  final void Function() onPressed;
+  final String text;
+
+  const CustomButton({
+    Key? key,
+    required this.width,
+    required this.height,
+    this.color = AppColors.elementblue,
+    required this.onPressed,
+    required this.text,
+  }) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          primary: color,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.r),
+          ),
+        ),
+        child: Text(
+          text,
+          style: AppTextStyles.bold18,
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
