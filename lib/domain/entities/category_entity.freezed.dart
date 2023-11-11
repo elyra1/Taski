19aa697ab033bb0cad6fc19bb28dbe1503f8861e
elyra1 <afonchenkov98@bk.ru>
2023-11-11 21:45:11 +0300

    Добавил модели(могут в будущем обновиться), заготовки под репозитории

---
+++ lib/domain/entities/category_entity.freezed.dart
@@ -0,0 +1,226 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'category_entity.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$CategoryEntity {
+  String get title => throw _privateConstructorUsedError;
+  String get id => throw _privateConstructorUsedError;
+  String get authorId => throw _privateConstructorUsedError;
+  int get color => throw _privateConstructorUsedError;
+  List<TaskEntity> get tasks => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $CategoryEntityCopyWith<CategoryEntity> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $CategoryEntityCopyWith<$Res> {
+  factory $CategoryEntityCopyWith(
+          CategoryEntity value, $Res Function(CategoryEntity) then) =
+      _$CategoryEntityCopyWithImpl<$Res, CategoryEntity>;
+  @useResult
+  $Res call(
+      {String title,
+      String id,
+      String authorId,
+      int color,
+      List<TaskEntity> tasks});
+}
+
+/// @nodoc
+class _$CategoryEntityCopyWithImpl<$Res, $Val extends CategoryEntity>
+    implements $CategoryEntityCopyWith<$Res> {
+  _$CategoryEntityCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? title = null,
+    Object? id = null,
+    Object? authorId = null,
+    Object? color = null,
+    Object? tasks = null,
+  }) {
+    return _then(_value.copyWith(
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      authorId: null == authorId
+          ? _value.authorId
+          : authorId // ignore: cast_nullable_to_non_nullable
+              as String,
+      color: null == color
+          ? _value.color
+          : color // ignore: cast_nullable_to_non_nullable
+              as int,
+      tasks: null == tasks
+          ? _value.tasks
+          : tasks // ignore: cast_nullable_to_non_nullable
+              as List<TaskEntity>,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$CategoryEntityImplCopyWith<$Res>
+    implements $CategoryEntityCopyWith<$Res> {
+  factory _$$CategoryEntityImplCopyWith(_$CategoryEntityImpl value,
+          $Res Function(_$CategoryEntityImpl) then) =
+      __$$CategoryEntityImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String title,
+      String id,
+      String authorId,
+      int color,
+      List<TaskEntity> tasks});
+}
+
+/// @nodoc
+class __$$CategoryEntityImplCopyWithImpl<$Res>
+    extends _$CategoryEntityCopyWithImpl<$Res, _$CategoryEntityImpl>
+    implements _$$CategoryEntityImplCopyWith<$Res> {
+  __$$CategoryEntityImplCopyWithImpl(
+      _$CategoryEntityImpl _value, $Res Function(_$CategoryEntityImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? title = null,
+    Object? id = null,
+    Object? authorId = null,
+    Object? color = null,
+    Object? tasks = null,
+  }) {
+    return _then(_$CategoryEntityImpl(
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      authorId: null == authorId
+          ? _value.authorId
+          : authorId // ignore: cast_nullable_to_non_nullable
+              as String,
+      color: null == color
+          ? _value.color
+          : color // ignore: cast_nullable_to_non_nullable
+              as int,
+      tasks: null == tasks
+          ? _value._tasks
+          : tasks // ignore: cast_nullable_to_non_nullable
+              as List<TaskEntity>,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$CategoryEntityImpl implements _CategoryEntity {
+  const _$CategoryEntityImpl(
+      {required this.title,
+      required this.id,
+      required this.authorId,
+      required this.color,
+      final List<TaskEntity> tasks = const []})
+      : _tasks = tasks;
+
+  @override
+  final String title;
+  @override
+  final String id;
+  @override
+  final String authorId;
+  @override
+  final int color;
+  final List<TaskEntity> _tasks;
+  @override
+  @JsonKey()
+  List<TaskEntity> get tasks {
+    if (_tasks is EqualUnmodifiableListView) return _tasks;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_tasks);
+  }
+
+  @override
+  String toString() {
+    return 'CategoryEntity(title: $title, id: $id, authorId: $authorId, color: $color, tasks: $tasks)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$CategoryEntityImpl &&
+            (identical(other.title, title) || other.title == title) &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.authorId, authorId) ||
+                other.authorId == authorId) &&
+            (identical(other.color, color) || other.color == color) &&
+            const DeepCollectionEquality().equals(other._tasks, _tasks));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, title, id, authorId, color,
+      const DeepCollectionEquality().hash(_tasks));
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$CategoryEntityImplCopyWith<_$CategoryEntityImpl> get copyWith =>
+      __$$CategoryEntityImplCopyWithImpl<_$CategoryEntityImpl>(
+          this, _$identity);
+}
+
+abstract class _CategoryEntity implements CategoryEntity {
+  const factory _CategoryEntity(
+      {required final String title,
+      required final String id,
+      required final String authorId,
+      required final int color,
+      final List<TaskEntity> tasks}) = _$CategoryEntityImpl;
+
+  @override
+  String get title;
+  @override
+  String get id;
+  @override
+  String get authorId;
+  @override
+  int get color;
+  @override
+  List<TaskEntity> get tasks;
+  @override
+  @JsonKey(ignore: true)
+  _$$CategoryEntityImplCopyWith<_$CategoryEntityImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
