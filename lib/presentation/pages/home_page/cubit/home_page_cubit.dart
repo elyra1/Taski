d4d8ac91b4816d5a4a9188632ecac592ebb22b3c
pogromartem <pogromartem@ya.ru>
2024-05-01 21:26:08 +0300

    добавил отображение фотографии другого пользователя

--- lib/presentation/pages/home_page/cubit/home_page_cubit.dart
+++ lib/presentation/pages/home_page/cubit/home_page_cubit.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
+import 'package:taski/domain/entities/user_model.dart';
 import 'package:taski/domain/repositories/user_repository.dart';
 
 part 'home_page_state.dart';
@@ -8,9 +9,16 @@ part 'home_page_cubit.freezed.dart';
 
 @injectable
 class HomePageCubit extends Cubit<HomePageState> {
-  final UserRepository _authRepository;
-  HomePageCubit(this._authRepository) : super(const HomePageState.initial());
-  Future<void> signOut() async {
-    await _authRepository.signOut();
+  final UserRepository _userRepository;
+  HomePageCubit(this._userRepository)
+      : super(HomePageState.initial(user: UserModel.getEmpty()));
+
+  Future<void> init({UserModel? user}) async {
+    if (user != null) {
+      emit(state.copyWith(user: user));
+    } else {
+      final currentUser = await _userRepository.getUser();
+      emit(state.copyWith(user: currentUser));
+    }
   }
 }
--- lib/presentation/pages/home_page/cubit/home_page_state.dart
+++ lib/presentation/pages/home_page/cubit/home_page_state.dart
@@ -2,5 +2,5 @@ part of 'home_page_cubit.dart';
 
 @freezed
 class HomePageState with _$HomePageState {
-  const factory HomePageState.initial() = _Initial;
+  const factory HomePageState.initial({required UserModel user}) = _Initial;
 }
